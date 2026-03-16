fa955f30ce365446ed159adeee67191752030860
ranger235 <ranger235@gmail.com>
2026-03-16 11:49:26 -0400

    Create sessionSequence.ts

---
+++ src/lib/sessionSequence.ts
@@ -0,0 +1,6 @@
+export const DEFAULT_SEQUENCE = ["Push","Pull","Lower"];
+
+export const USER_SEQUENCES: Record<string,string[]> = {
+  dave: ["Push","Pull","Lower"],
+  tim: ["Chest","Back","Shoulders","Biceps","Triceps","Legs"]
+};
