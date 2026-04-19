797255b8d9550507b7a0838a83e4515a9b177db5
ranger235 <ranger235@gmail.com>
2026-04-19 00:52:01 -0400

    Update exerciseControl.ts

--- src/lib/exerciseControl.ts
+++ src/lib/exerciseControl.ts
@@ -1,7 +1,22 @@
-export type ExerciseControl = {
+export type ExerciseControlRec = {
+  user_id: string;
   exercise_library_id: string;
-  prefer?: boolean;
-  avoid?: boolean;
-  never?: boolean;
-  injury?: boolean;
+  prefer: boolean;
+  avoid: boolean;
+  never: boolean;
+  injury: boolean;
+  updated_at: string;
 };
+
+export function emptyExerciseControl(user_id: string, exercise_library_id: string): ExerciseControlRec {
+  return {
+    user_id,
+    exercise_library_id,
+    prefer: false,
+    avoid: false,
+    never: false,
+    injury: false,
+    updated_at: new Date().toISOString(),
+  };
+}
+
